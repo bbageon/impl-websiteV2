8da1c968b924a0fb00316077d48a2bf19d228c68
BBageon <rjsdn4403@gmail.com>
2024-08-13 20:17:24 +0900

    [feat] spring-react setting complete

---
+++ src/main/java/com/example/implwebsitev2/ImplWebsiteV2Application.java
@@ -0,0 +1,13 @@
+package com.example.implwebsitev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplWebsiteV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplWebsiteV2Application.class, args);
+    }
+
+}
